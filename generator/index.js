21851eab72894d11c5bb47f741f6ff1059dfce47
Jaco Greeff <jacogr@gmail.com>
2016-06-10 18:19:20 +0200

    generator

--- generator/index.js
+++ generator/index.js
@@ -1,46 +1,37 @@
-#!/usr/bin/env node
-'use strict';
+import fs from 'fs';
+import path from 'path';
 
-// Depracated, make changes directly to rpc.json file
+import interfaces from '../lib';
 
-const customFunctions = require('./custom-functions');
-const addFormatters = require('./add-formatters');
-const format = require('./format');
+const functions = [];
 
-const fs = require('fs');
-const nl = /\n+/;
-const OUTPUT_PATH = `${__dirname}/generator-output/rpc.json`;
+Object.keys(interfaces).forEach((group) => {
+  interfaces[group].forEach((method) => {
+    method.name = `${group}_${method.name}`;
+    functions.push(method);
+  });
+});
 
-const val = fs.readFileSync(`${__dirname}/docs.md`, 'utf8');
-const funcs = val.split(/\s#### /)
-  // Skip first element (intro docs)
-  .slice(1)
-  // Extract data
-  .map(parseSingleMethod)
-  // Add our custom functions
-  .concat(customFunctions)
-  // Add formatters
-  .map(addFormatters);
+console.log(JSON.stringify({
+  methods: functions.sort((a, b) => {
+    if (a.name < b.name) {
+      return -1;
+    } else if (a.name > b.name) {
+      return 1;
+    }
 
-// console.log(funcs);
-fs.writeFileSync(OUTPUT_PATH, JSON.stringify({
-  methods: funcs
+    return 0;
+  })
+}, null, 2));
+
+fs.writeFileSync(path.join(__dirname, '../index.json'), JSON.stringify({
+  methods: functions.sort((a, b) => {
+    if (a.name < b.name) {
+      return -1;
+    } else if (a.name > b.name) {
+      return 1;
+    }
+
+    return 0;
+  })
 }, null, 2), 'utf8');
-
-console.log(`success generated json at ${OUTPUT_PATH}`);
-
-function parseSingleMethod (data) {
-  const parts = data.split('##### ');
-  let name = parts[0].split(nl)[0];
-  let desc = nonEmpty(parts[0].split(nl).slice(1)).join('\n');
-  let params = format.params(nonEmpty(parts[1].split(nl).slice(1)));
-  let returns = format.returns(nonEmpty(parts[2].split(nl).slice(1)));
-
-  return {
-    name, desc, params, returns
-  };
-}
-
-function nonEmpty (a) {
-  return a.filter(x => x);
-}
