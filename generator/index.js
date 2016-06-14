48524a9a700e2f80cec380f77ebf37eb21238ba3
Jaco Greeff <jacogr@gmail.com>
2016-06-14 10:04:00 +0200

    use built-in & extended types

--- generator/index.js
+++ generator/index.js
@@ -4,21 +4,23 @@ import path from 'path';
 import interfaces from '../lib';
 
 const INDEX_JSON = path.join(__dirname, '../index.json');
-
 const methods = [];
+
+function formatType (obj) {
+  if (!obj || !obj.type || !obj.type.name) {
+    return obj;
+  }
+
+  return `${obj.type.name} - ${obj.desc}`;
+}
 
 Object.keys(interfaces).sort().forEach((group) => {
   Object.keys(interfaces[group]).sort().forEach((name) => {
     const method = interfaces[group][name];
 
     method.name = `${group}_${name}`;
-    method.params = method.params.map((param) => {
-      if (!param.desc && !param.type) {
-        return param;
-      }
-
-      return `${param.type} - ${param.desc}`;
-    });
+    method.params = method.params.map(formatType);
+    method.returns = formatType(method.returns);
 
     methods.push(method);
   });
