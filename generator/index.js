b85012aabc7ceed6feca5032b8861d260cad8be5
Jaco Greeff <jacogr@gmail.com>
2016-06-14 12:03:44 +0200

    type formatting for objects

--- generator/index.js
+++ generator/index.js
@@ -7,11 +7,22 @@ const INDEX_JSON = path.join(__dirname, '../index.json');
 const methods = [];
 
 function formatType (obj) {
-  if (!obj || !obj.type || !obj.type.name) {
-    return obj;
+  if (obj.type === Object && obj.details) {
+    const formatted = {};
+
+    Object.keys(obj.details).sort().forEach((key) => {
+      formatted[key] = formatType(obj.details[key]);
+    });
+
+    return {
+      description: `${obj.type.name} - ${obj.desc}`,
+      details: formatted
+    };
+  } else if (obj.type && obj.type.name) {
+    return `${obj.type.name} - ${obj.desc}`;
   }
 
-  return `${obj.type.name} - ${obj.desc}`;
+  return obj;
 }
 
 Object.keys(interfaces).sort().forEach((group) => {
