1569c3b1bdd803dd2ce449a62283997cbcc91604
Jaco Greeff <jacogr@gmail.com>
2016-06-15 10:21:56 +0200

    markdown generator for new interface

--- generator/build-markdown.js
+++ generator/build-markdown.js
@@ -10,6 +10,27 @@ const BLOCK_END = '```';
 let preamble = '# interfaces\n';
 let markdown = '';
 
+function formatDescription (obj, prefix = '', indent = '') {
+  const optional = obj.optional ? '(optional) ' : '';
+  const defaults = obj.default ? `(default: ${obj.default}) ` : '';
+
+  return `${indent}- ${prefix}\`${obj.type.name}\` - ${optional}${defaults}${obj.desc}`;
+}
+
+function formatType (obj) {
+  if (obj.type === Object && obj.details) {
+    const sub = Object.keys(obj.details).sort().map((key) => {
+      return formatDescription(obj.details[key], `\`${key}\`/`, '    ');
+    }).join('\n');
+
+    return `${formatDescription(obj)}\n${sub}`;
+  } else if (obj.type && obj.type.name) {
+    return formatDescription(obj);
+  }
+
+  return obj;
+}
+
 Object.keys(interfaces).sort().forEach((group) => {
   let content = '';
 
@@ -20,18 +41,11 @@ Object.keys(interfaces).sort().forEach((group) => {
     const method = interfaces[group][iname];
     const name = `${group}_${iname}`;
     const desc = method.desc;
-    const params = method.params.map((param) => {
-      switch (Object.prototype.toString.call(param)) {
-        case '[object String]':
-          return `- ${param}`;
-        default:
-          return `- ${BLOCK_BEGIN}${JSON.stringify(param, null, 2)}${BLOCK_END}`;
-      }
-    });
-    const returns = (Object.prototype.toString.call(method.returns) === '[object String]' ? method.returns : `${BLOCK_BEGIN}${JSON.stringify(method.returns, null, 2)}${BLOCK_END}`) || 'none';
+    const params = method.params.map(formatType).join('\n');
+    const returns = formatType(method.returns);
 
     markdown = `${markdown}\n- [${name}](#${name})`;
-    content = `${content}### ${name}\n\n${desc}\n\n#### parameters\n\n${params.join('\n') || 'none'}\n\n#### returns\n\n${returns}\n\n`;
+    content = `${content}### ${name}\n\n${desc}\n\n#### parameters\n\n${params || 'none'}\n\n#### returns\n\n${returns || 'none'}\n\n`;
   });
 
   markdown = `${markdown}\n\n${content}`;
